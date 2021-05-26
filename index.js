411d05cf3b82ef00f17fbc82ca7a3732747776aa
LOVE VAISHNAV <lovevaishnav47@gmail.com>
2021-05-27 00:51:40 +0530

    new changee

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 let port = process.env.port || 8000;
 const importData = require("./sellerdata.json");
 
 app.get("/" , (req, res) =>{
-    res.send(importData);
+    fs.readFile(`${__dirname}/sellerdata.json` , "utf-8" , (err, data) =>{
+        console.log(data);
+        const obj = JSON.parse(data);
+        res.send(obj);
+    });
+    
+   
+    
 });
+
+console.log(__dirname);
 
 
 app.listen(port , () =>{
